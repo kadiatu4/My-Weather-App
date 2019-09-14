75a0fb0d842f446a2a4a44174274fdd034c16ea6
Kadiatou Diallo <Kadiatu4@gmail.com>
2019-09-14 14:31:12 -0400

    added divs around Styled component Elements

--- src/component/Form.js
+++ src/component/Form.js
@@ -4,25 +4,26 @@ import styled from "styled-components";
 
 const BannerContainer = styled.div`
     display: flex;
-    
-    .texts{
-        margin: 2rem 2rem;
-    
-        h1{
-            color: rgb(76, 166, 245);
-            font-weight: 300;
-            font-size: 2rem;   
-        }
-        h2{
-            color: white;
-            font-weight: 200;
-            font-size: 1.3rem;   
-        }
-        p{
-           color: white;
-           font-weight: 100;
-        }
-    
+`;
+
+const Titles = styled.div`
+    margin: 2rem 2rem;
+        
+    h1{
+        color: rgb(76, 166, 245);
+        font-weight: 300;
+        font-size: 2rem;   
+    }
+
+    h2{
+        color: white;
+        font-weight: 200;
+        font-size: 1.3rem;   
+    }
+
+    p{
+        color: white;
+        font-weight: 100;
     }
 `;
 
@@ -32,6 +33,7 @@ const FormContainer = styled.div`
     .btn{
         background-color: #ffbb33;
         margin-left: 2rem;
+        color: white;
     }
 
     input, 
@@ -51,31 +53,36 @@ const FormContainer = styled.div`
 const Form = (props) => {
 
     return(
-        <BannerContainer class="row">
-            <div class="col-7 texts"> 
-                <h1>My Weather App </h1>
-                <h2>5 Day 3 Hour Forecast</h2>
-                <p>Enter a <strong>Zipcode</strong> or <strong>City, Country </strong>and we will provide you with a
-                    5-day weather forecast for that location.
-                </p>
-            </div>
-            
-            <FormContainer class="col-5">
+        <BannerContainer>
+            <div class="row">
 
-                <div class="form-group"> 
-
-                    <form onSubmit={props.loadWeather}>
-                        <div class="form-inline">  
-                            <input type="text" class="inputContainer form-control form-control" name="location" placeholder="Enter city and country, or zipcode " />
-                            <button type="submit" class="btn">Get Weather</button>
-                            
-                        </div>
-                    </form>
-
+                <div class="col-7">
+                    <Titles> 
+                        <h1>My Weather App </h1>
+                        <h2>5 Day 3 Hour Forecast</h2>
+                        <p>Enter a <strong>Zipcode</strong> or <strong>City, Country </strong>and we will provide you with a
+                            5-day weather forecast for that location.
+                        </p>
+                    </Titles>
                 </div>
-               
-            </FormContainer>
             
+                <div class="col-5">
+                    <FormContainer>
+
+                        <div class="form-group"> 
+
+                            <form onSubmit={props.loadWeather}>
+                                <div class="form-inline">  
+                                    <input type="text" class="form-control" name="location" placeholder="Enter city and country, or zipcode " />
+                                    <button type="submit" class="btn">Get Weather</button>
+                                </div>
+                            </form>
+
+                        </div>
+
+                    </FormContainer>
+                </div>
+            </div>
         </BannerContainer> 
     )
 
